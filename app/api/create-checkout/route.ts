e6acdd8636e8eebbcb7b54f7d4ef8b4323f9be4b
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2026-02-06 09:48:13 +0000

    feat: Torna possível o uso de cupons de desconto no checkout da stripe

--- app/api/create-checkout/route.ts
+++ app/api/create-checkout/route.ts
@@ -36,6 +36,7 @@ export async function POST(req: Request) {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       payment_method_types: ["card", "mb_way", "multibanco"],
+      allow_promotion_codes: true,
       line_items: [
         {
           price: priceId,
